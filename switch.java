df6507fef439105bbe5d4446dfade53f546dcfa1
Garima sondhiya <garimasondhiya2608@gmail.com>
2024-06-25 19:21:25 +0530

    Add files via upload

---
+++ switch.java
@@ -0,0 +1,19 @@
+public class switch {
+    public static void main(String[] args) {
+        int number = 3;
+        switch (number) {
+            case 1:
+                System.out.println("burger");
+                break;
+            case 2:
+                System.out.println("pizza");
+                break;
+            case 3:
+                System.out.println("momos");
+                break;
+            default:
+                System.out.print("nothing");
+
+        }
+    }
+}
