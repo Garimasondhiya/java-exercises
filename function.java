57dfe55f229ed8863fc6f5365c76fef9740e786d
Garima sondhiya <garimasondhiya2608@gmail.com>
2024-06-28 21:45:24 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class function{
+
+ public static void calculateSum(){
+ Scanner Sc= new Scanner(System.in);
+int a = Sc.nextInt();
+int b = Sc.nextInt();
+int sum= a+b ;
+System.out.println(sum);
+ }
+
+    public static void main(String args[]){
+        calculateSum();
+    }
+
+
+
+}
+
+
+
+
+
+
+
