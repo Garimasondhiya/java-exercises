62156ec66959007db201199b43b7460c387ee531
Garima sondhiya <garimasondhiya2608@gmail.com>
2024-07-16 22:24:09 +0530

    Add files via upload

---
+++ arrey.java
@@ -0,0 +1,19 @@
+/**
+ * arrey
+ */
+public class arrey {
+
+    
+    public static int sum(int a,int b){
+             
+             return a+b ;
+
+    }
+    public static void main(String args[]){
+        int a = 4;
+        int b= 5;
+       
+        sum (a,  b);
+    }
+    
+}
