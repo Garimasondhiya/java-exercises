12c8f3f28e9f00609cc8f942b3e8003740b30031
Garima sondhiya <garimasondhiya2608@gmail.com>
2024-07-26 14:21:00 +0530

    Add files via upload

--- function.java
+++ function.java
@@ -1,25 +1,60 @@
-import java.util.*;
-public class function{
+// public class function {
+    
+ 
 
- public static void calculateSum(){
- Scanner Sc= new Scanner(System.in);
-int a = Sc.nextInt();
-int b = Sc.nextInt();
-int sum= a+b ;
-System.out.println(sum);
- }
+//         private static final String intb = null;
 
-    public static void main(String args[]){
-        calculateSum();
-    }
-
-
-
-}
-
+//         public static int Sum(int a, int b){           // parameter
+                 
+//                  int c;
+//                  if( a< b){
+//                      c = a+b;
+//                  }
+//                  else{                           // logic define
+//                     c= a-b;
+//                  }
+                     
+//                return c ;            
 
 
+//          }
+
+//                public static void main(String[] args, Object c, Object inta) {
+//                          int a = 3;
+//                          int b= 5;
+//                          c =Sum(inta,intb);
+                       
+     
+
+                         public class function {
+
+                            public static int Sum(int a, int b) {  // parameter
+                                int c;
+                                if (a < b) {
+                                    c = (a + b)*a;
+                                } else {  // logic define
+                                    c = (a - b)*b;
+                                }
+                                return c;  // 8
+                            }
+                        
+                            public static void main(String[] args) {
+                                int a = 3;
+                                int b = 5;
+                                int result = Sum(a, b);  // call Sum method and store the result
+                                System.out.println("The result is: " + result);  // print the result
+                            }
+                        }
+                        
 
 
+
+
+
+
+
+               
+
+           
 
 
