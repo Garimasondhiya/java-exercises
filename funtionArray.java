d961cd6e90fdf7491112813b420784bfa89e792a
Garima sondhiya <garimasondhiya2608@gmail.com>
2024-07-29 23:30:48 +0530

    Add files via upload

---
+++ funtionArray.java
@@ -0,0 +1,18 @@
+public class funtionArray {
+    public static void logic (int marks[]){
+           for(int i= 0; i< marks.length; i++){
+           }    
+           }
+            public static void main (String args[]){
+                 int marks[]= {33,78,58,85};
+                    logic( marks);
+                 // print our marks
+                 for(int i= 0; i< marks.length; i++){
+                System.out.println( marks[i]);
+                }
+              
+
+            }
+
+    }
+
