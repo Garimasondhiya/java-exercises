51bbbaa0895dd346795af0333cea745554df673e
Garima sondhiya <garimasondhiya2608@gmail.com>
2024-07-29 22:25:35 +0530

    Add files via upload

---
+++ outputArray.java
@@ -0,0 +1,45 @@
+
+public class outputArray{
+// output in array
+public static void main(String[] args) {
+    // int marks[]=  new int [3];
+    // marks[0]= 55;
+    // marks[1]= 66;
+    // marks[2]= 33;
+    // // System.out.println(marks[0]);
+    // // System.out.println(marks[1]);
+    // // System.out.println(marks[2]);
+    //  for(int i = 0; i<=3;i++){
+    //     System.out.println(marks[i]);
+    
+//     int marks[]= {100, 44, 24,44,144,};
+// System.out.println( marks[0]);
+// System.out.println( marks[1]);
+// System.out.println( marks[2]);
+// System.out.println( marks[3]);
+// System.out.println( marks[4]);
+//         }
+
+
+
+    String name[]= {"rocky", "jack", "elon", "das"};
+             for(int i = 0; i< name.length; i++){
+           System.out.println(name[i]);
+}
+    
+           
+}
+
+    // public static void main(String[] args) {
+    
+
+}
+
+
+
+
+
+
+ 
+
+
