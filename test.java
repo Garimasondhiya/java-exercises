d27f310b720ce95f928529c2504020899627de2c
Garima sondhiya <garimasondhiya2608@gmail.com>
2024-08-13 18:55:26 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,32 @@
+/**
+ * test
+ */
+public class test {
+
+    public static void main(String[] args) {
+        // for(int i = 1; i<=5;i++){
+    
+            // for( int j = 1; j<= 5-i; j++){
+            //      System.out.print(" ");
+            //     }
+            //   // star print
+            //   for( int j = 1; j<=i; j++){
+            //     System.out.print(j);
+            //   }
+            //     System.out.println();
+            // //          
+// triangle pattern
+                    int n = 5;
+                    for(int i = 1; i<=n; i++){
+                           for(int j= 1; j<=n-i ; j++){
+                            System.out.print(" ");
+                           }
+                        for(int j = 1; j<=i; j++){
+                          System.out.print("*");
+                        }
+             System.out.println();
+                    }
+             }
+
+        }
+    
