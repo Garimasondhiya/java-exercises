60234658dddec5b8547f096cd72c97cdd6b2d535
Garima sondhiya <garimasondhiya2608@gmail.com>
2024-08-15 10:08:09 +0530

    Add files via upload

---
+++ suminArray.java
@@ -0,0 +1,16 @@
+/**
+ * suminArray
+ */
+public class suminArray {
+
+    public static void main(String[] args) {
+        int array[]={4,7,4,24,23,78};
+        int sum= 0;
+        for(int i = 0; i<array.length; i++){
+           sum += array[i];
+
+        }
+    System.out.println(sum);
+    }
+    
+}
